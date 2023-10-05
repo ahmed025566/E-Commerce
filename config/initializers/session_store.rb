a5f8d3839f976aae7b9fb02494bff1bcf13bb23c
ahmed025566 <ahmedsalah025566@gmail.com>
2023-10-05 19:59:01 +0300

    Setup rails

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, key: '_E_Commerce_app',
+                                                          domain: 'authentication-app-api.herokuapp.com'
+  else
+    Rails.application.config.session_store :cookie_store, key: '_E_Commerce_app'
+  end
+  
