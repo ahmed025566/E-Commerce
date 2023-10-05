8b66cb68b1b6130b2cfed22be4d0a31654ec293e
ahmed025566 <ahmedsalah025566@gmail.com>
2023-10-05 21:22:30 +0300

    Add cart model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+  has_one :cart
 
   validates_presence_of :email
   validates_presence_of :email
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_005_181_535) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_181827) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
+
+  add_foreign_key "carts", "users"
 end
